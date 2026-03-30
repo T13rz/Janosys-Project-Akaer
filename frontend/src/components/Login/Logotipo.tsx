0a0dea04ca59577122ed42bd2c479346aee7d147
Vinicius Moreira <vinnismoreira@icloud.com>
2026-03-30 00:18:24 +0000

    fix: imagem da logo da pagina de login

--- frontend/src/components/Login/Logotipo.tsx
+++ frontend/src/components/Login/Logotipo.tsx
@@ -1,13 +1,9 @@
-import logoAkaer from '../../assets/akaer30.png';
+import logoAkaerSigna from '../../assets/AKAER.signa.texto.png';
 
 function Logotipo() {
   return (
     <div className="container-logotipo">
-      <div className="cabecalho-logotipo">
-        <img src={logoAkaer} alt="Akaer" className="logo-akaer" />
-        <div className="emblema-signa">SIGNA</div>
-      </div>
-      <p className="subtitulo-logotipo">Sistema Integrado de Gestão de Normativas Aeronáuticas</p>
+      <img src={logoAkaerSigna} alt="Akaer Signa" className="logo-akaer-signa" />
     </div>
   );
 }
